a59afeab77c4c7b89364c0eda2ba156e2be8035c
alexbyk <alex@alexbyk.com>
2018-06-24 20:09:05 +0300

    feat(front): change id feature

--- front/src/app/pages/manage/manage.component.ts
+++ front/src/app/pages/manage/manage.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '@src/app/api/api.service';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-manage',
@@ -7,7 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageComponent implements OnInit {
 
-  constructor() { }
+  constructor(public api: ApiService, public snackBar: MatSnackBar) {
+    this.id = api.id;
+  }
+
+  id: string;
+
+  changeId() {
+    this.api.changeId(this.id);
+    this.snackBar.open(`You id has been changed`, null, { duration: 500, panelClass: 'snack-success' });
+  }
 
   ngOnInit() {
   }
